18648c40d93c9a6a5132cac5e63ebd75b0fec61a
Ethan Lee <ethanlee@ethansites.com>
2017-03-07 09:12:45 +0000

    second commit

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const bodyParser = require('body-parser');
 const request = require('request-promise');
 const app = express();
 
+const oauth = {
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  token: process.env.TWITTER_TOKEN,
+  token_secret: process.env.TWITTER_TOKEN_SECRET,
+  transport_method: 'query'
+};
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -12,8 +20,27 @@ app.get('/', (req, res, next) => {
 });
 
 app.post('/api/data', (req, res, next) => {
-  console.log(req.body);
-  res.json({});
+  const lat = req.body.latitude;
+  const long = req.body.longitude;
+  const results = {};
+  request(`https://api.darksky.net/forecast/` 
+  + `${process.env.DARKSKY_KEY}/${lat},${long}`, {
+    json: true
+  })
+  .then((response) => {
+    results.weather = response.currently;
+    return request(`https://api.twitter.com/1.1/search/tweets.json?q=Teen%20Tech%20Week`,
+      { oauth, json: true });
+    return Promise.resolve(true);
+  })
+  .then((response) => {
+    results.twitter = response.statuses;
+  })
+  .then(() => {
+    console.log(results);
+    res.json(results);
+  })
+  .catch(err => next(err));
 });
 
 app.use((err, req, res, next) => {
@@ -23,4 +50,4 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT, process.env.IP);
--- public/js/client.js
+++ public/js/client.js
@@ -1,23 +1,28 @@
+let rowIndex = 0;
+
 function init() {
  appLoop();
  navigator.geolocation.getCurrentPosition((position) => {
    console.log(position);
-   fetch('/api/data', {
-     method: 'POST',
-     headers: {
-       Accept: 'application/json',
-       'Content-Type': 'application/json'
-     },
-     body: JSON.stringify({
-       latitude: position.coords.latitude,
-       longitude: position.coords.longitude
-     })
-   })
+  fetch('/api/data', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    })
+  })
+  .then(response => response.json())
+  .then(response => update(response))
+  .catch(err => alert(err));
  });
  const mainInterval = setInterval(appLoop, 1000);
- setTimeout(() => {
-   window.location = window.location;
- }, 10000);
+// setTimeout(() => {
+//   window.location = window.location;
+// }, 10000);
 }
 
 function appLoop() {
@@ -25,8 +30,61 @@ function appLoop() {
   $('#date').html(moment(Date.now()).format('LL'));
 }
 
-function update() {
+function scroll(direction) {
+  const content = $('.scrollable').first();
   
+  if ((rowIndex + 1 >= content.children().length && direction === 'down')
+  || (rowIndex - 1 < 0) && direction === 'up') {
+    return;
+  }
+  
+  const currentRow = $('.contentrow.selected').first();
+  currentRow.removeClass('selected');
+  rowIndex = direction === 'down' ? rowIndex + 1 : rowIndex - 1;
+  const delta = direction === 'down' ? currentRow.outerHeight() : -1 * currentRow.outerHeight();
+  
+  $(content.children().get(rowIndex)).addClass('selected');
+  console.log('scrolling to ', delta, content.scrollTop())
+  
+  // content.scroll();
+  content.animate({
+    scrollTop: delta + content.scrollTop()
+  }, 1000);
+}
+
+// update(results<Object>)
+// Updates HTML contents with results object returned
+// from server. Will include the following properties:
+// { weather: {...}, twitter: {...}, ...}
+function update(results) {
+  console.log(results);
+  $('#weather-num').html(results.weather.temperature);
+  let tweets = results.twitter;
+  const rows = tweets.map(function(tweet, i) {
+    return `<div class="row contentrow ${i === 0 ? 'selected' : ''}">
+      <div class="col-xs-1">
+        <img class="" src="${tweet.user.profile_image_url_https}">
+      </div>
+      <div class="col-xs-8">
+        <p class="heading">${tweet.user.name} (@${tweet.user.screen_name})</p>
+        <p class="content">${tweet.text}</p>
+      </div>
+    </div>`
+  });
+  $('.scrollable').first().html(rows);
 }
 
 init();
+
+const icons = {
+  'clear-day': 'fa-sun-o',
+  'clear-night': 'fa-moon-o',
+  'rain': 'fa-tint',
+  'snow': 'fa-snowflake-o',
+  'sleet': 'fa-snowflake-o',
+  'wind': 'fa-skyatlas',
+  'fog': 'fa-soundcloud',
+  'cloudy': 'fa-cloud',
+  'partly-cloudy-day': 'fa-soundcloud',
+  'partly-cloudy-night': 'fa-soundcloud'
+}
