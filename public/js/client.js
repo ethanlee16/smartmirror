6aeb168a8afff565eb6c9cd4fe9efb933d000dcb
Ethan Lee <ethanlee@ethansites.com>
2017-03-07 10:15:02 +0000

    leap motion

--- public/js/client.js
+++ public/js/client.js
@@ -1,9 +1,21 @@
 let rowIndex = 0;
+const icons = {
+  'clear-day': 'fa-sun-o',
+  'clear-night': 'fa-moon-o',
+  'rain': 'fa-tint',
+  'snow': 'fa-snowflake-o',
+  'sleet': 'fa-snowflake-o',
+  'wind': 'fa-skyatlas',
+  'fog': 'fa-soundcloud',
+  'cloudy': 'fa-cloud',
+  'partly-cloudy-day': 'fa-soundcloud',
+  'partly-cloudy-night': 'fa-soundcloud'
+}
 
 function init() {
  appLoop();
  navigator.geolocation.getCurrentPosition((position) => {
-   console.log(position);
+  console.log(position);
   fetch('/api/data', {
     method: 'POST',
     headers: {
@@ -20,6 +32,30 @@ function init() {
   .catch(err => alert(err));
  });
  const mainInterval = setInterval(appLoop, 1000);
+ const controller = Leap.loop({
+   enableGestures: true,
+   host: '192.168.2.100',
+   port: '8001'
+ }, function(frame) {
+  if (frame.valid && frame.gestures.length > 0) {
+    frame.gestures.forEach(function(gesture) {
+      switch (gesture.type){
+        case "circle":
+          console.log("Circle Gesture", gesture);
+          break;
+        case "keyTap":
+          console.log("Key Tap Gesture");
+          break;
+        case "screenTap":
+          console.log("Screen Tap Gesture");
+          break;
+        case "swipe":
+          console.log("Swipe Gesture");
+          break;
+      }
+    });
+  }
+ });
 // setTimeout(() => {
 //   window.location = window.location;
 // }, 10000);
@@ -46,7 +82,6 @@ function scroll(direction) {
   $(content.children().get(rowIndex)).addClass('selected');
   console.log('scrolling to ', delta, content.scrollTop())
   
-  // content.scroll();
   content.animate({
     scrollTop: delta + content.scrollTop()
   }, 1000);
@@ -56,9 +91,12 @@ function scroll(direction) {
 // Updates HTML contents with results object returned
 // from server. Will include the following properties:
 // { weather: {...}, twitter: {...}, ...}
+
 function update(results) {
   console.log(results);
   $('#weather-num').html(results.weather.temperature);
+  $('#weather-icon').html(`<i class="fa ${icons[results.weather.icon]}"></i>`);
+  
   let tweets = results.twitter;
   const rows = tweets.map(function(tweet, i) {
     return `<div class="row contentrow ${i === 0 ? 'selected' : ''}">
@@ -69,22 +107,9 @@ function update(results) {
         <p class="heading">${tweet.user.name} (@${tweet.user.screen_name})</p>
         <p class="content">${tweet.text}</p>
       </div>
-    </div>`
+    </div>`;
   });
-  $('.scrollable').first().html(rows);
+  $('.twitter .scrollable').first().html(rows);
 }
 
 init();
-
-const icons = {
-  'clear-day': 'fa-sun-o',
-  'clear-night': 'fa-moon-o',
-  'rain': 'fa-tint',
-  'snow': 'fa-snowflake-o',
-  'sleet': 'fa-snowflake-o',
-  'wind': 'fa-skyatlas',
-  'fog': 'fa-soundcloud',
-  'cloudy': 'fa-cloud',
-  'partly-cloudy-day': 'fa-soundcloud',
-  'partly-cloudy-night': 'fa-soundcloud'
-}
